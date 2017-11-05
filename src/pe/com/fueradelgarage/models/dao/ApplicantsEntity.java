b5e0558909bc041d731e014d7716e22903e1db5a
Angelo1793 <angelo1793@hotmail.com>
2017-11-05 11:18:23 -0500

    Actualizacion de ApllicantEntity
    - Modificacion del codigo de acuerdo ak nuevo padre "User" creado en la base de datos

--- src/pe/com/fueradelgarage/models/dao/ApplicantsEntity.java
+++ src/pe/com/fueradelgarage/models/dao/ApplicantsEntity.java
@@ -1,6 +1,7 @@
 package pe.com.fueradelgarage.models.dao;
 
 import pe.com.fueradelgarage.models.dto.Applicant;
+import pe.com.fueradelgarage.models.dto.User;
 import sun.security.krb5.internal.APOptions;
 
 import java.sql.Connection;
@@ -13,14 +14,14 @@ public class ApplicantsEntity extends BaseEntity{
 
     public ApplicantsEntity(Connection connection, String tableName){
         super(connection,tableName);
+        setTableName("aplicants");
     }
 
     public ApplicantsEntity(){
-        super();
         setTableName("applicants");
     }
 
-    public List<Applicant> findByCriteria(String criteria){
+    public List<Applicant> findByCriteria(String criteria, UsersEntity usersEntity){
 
         try {
             ResultSet rs = getConnection()
@@ -30,7 +31,7 @@ public class ApplicantsEntity extends BaseEntity{
                             .concat(criteria));
            List<Applicant> applicants = new ArrayList<>();
            while(rs.next())
-               applicants.add(Applicant.from(rs));
+               applicants.add(Applicant.from(rs,usersEntity));
             return applicants;
 
         } catch (SQLException e) {
@@ -39,43 +40,44 @@ public class ApplicantsEntity extends BaseEntity{
           return null;
     }
 
-    public Applicant findById(String id){
+    public Applicant findById(int id,UsersEntity usersEntity){
         return findByCriteria(
-                String.format("WHERE id = '%s'", id)).get(0);
+                String.format("WHERE id_applicants = %d ", id),usersEntity).get(0);
     }
 
-    public Applicant findByName(String name){
+    public Applicant findByName(String name, UsersEntity usersEntity){
         return findByCriteria(
-                String.format("WHERE name = '%s'",name)).get(0);
+                String.format("WHERE name = '%s'",name),usersEntity).get(0);
     }
 
-    public List<Applicant> findAll(){ return findByCriteria("");}
+    public List<Applicant> findAll(UsersEntity usersEntity){
+        return findByCriteria("",usersEntity);}
 
     public boolean create(Applicant applicant){
         return executeUpdate(String.format(
-                "INSERT INTO %s(id,user,password,description,score,videoUrl) VALUES('%s','%s','%s','%s','%d','%s')",
-                getTableName(),applicant.getId(),applicant.getUser(),applicant.getPassword(),applicant.getScore(),applicant.getVideoUrl()));
+                "INSERT INTO %s(id_applicants,id_user,name,video_url) VALUES(%d,%d,'%s','%s')",
+                getTableName(),applicant.getId(),applicant.getUser().getId(),applicant.getName(),applicant.getVideoUrl()));
     }
 
-    public boolean create (String id, String user, String password, String description, int score, String videoUrl){
-            return create(new Applicant(id, user,password,description, score, videoUrl)); }
+    public boolean create (int id,String name ,String videoUrl,User user){
+            return create(new Applicant(id,name,videoUrl,user)); }
 
-    public boolean update(String id,String user, String password, String description, int score, String videoUrl){
+    public boolean update(int id,String name,String videoUrl,User user ){
             return executeUpdate(String.format(
-                    "UPDATE %s SET user = '%s', password = '%s', description = '%s',score = '%d', videoUrl = '%s'"));
+                    "UPDATE %s SET name = '%s',video_url='%s',id_user=%d WHERE id_applicants=%d",getTableName(),name,videoUrl,user,id ));
     }
 
     public boolean update (Applicant applicant ) {
         return update (applicant.getId(),
-                applicant.getUser(),applicant.getPassword(),applicant.getDescription(),applicant.getScore(),applicant.getVideoUrl());}
+                applicant.getName(),applicant.getVideoUrl(),applicant.getUser());}
 
-    public boolean erase (String id){
-        return executeUpdate(String.format("DELETE FROM '%s' WHERE id = '%s'",
+    public boolean erase (int id){
+        return executeUpdate(String.format("DELETE FROM '%s' WHERE id_applicants = %d",
                 getTableName(), id));
     }
 
     public boolean erase (Applicant applicant) {
-        return executeUpdate(String.format("DELETE FROM %s WHERE id = %s",
-            getTableName(), applicant.getId()));}
+        return executeUpdate(String.format("DELETE FROM %s WHERE id_applicants = %d",
+            getTableName(), applicant.getId(),applicant.getUser()));}
 
 }
